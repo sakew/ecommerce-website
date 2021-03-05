234f5234647e249ff49b2ee9afc8b9d4bdd12b6e
sakew <dorinsecu@gmail.com>
2021-03-05 15:57:34 +0200

    added HomeScreen and ProductScreen components and implemented Routes with react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,30 @@
 import React from "react";
-import data from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Ecommerce Website
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign in</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All Rights Reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Ecommerce Website
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign in</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
