58401086eaf3edff4effafe23079d630e6042ce7
sakew <dorinsecu@gmail.com>
2021-03-06 17:52:32 +0200

    added connection to mongoDB and create API to add users based on a userSchema + routes from userRouter.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect("mongodb://localhost/ecommerce-website", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((item) => item._id === req.params.id);
@@ -16,9 +23,16 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.use("/api/users", userRouter);
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+
+app.use((error, req, res, next) => {
+  res.status(500).send({ message: error.message });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
