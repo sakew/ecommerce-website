56392e6cb88c6ef6a7430a4757469e835929e113
sakew <dorinsecu@gmail.com>
2021-03-07 02:57:11 +0200

    added productModel and productRouter for dynamically gathering data from the mongodb database instead of data.js static method and added routes to it in the backend server.js

--- backend/data.js
+++ backend/data.js
@@ -18,7 +18,7 @@ const data = {
 
   products: [
     {
-      _id: "1",
+      // _id: "1",
       name: "Nike Slim Shirt",
       category: "Shirts",
       image: "/images/p1.jpg",
@@ -30,7 +30,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "2",
+      // _id: "2",
       name: "Adidas Fit Shirt",
       category: "Shirts",
       image: "/images/p2.jpg",
@@ -42,7 +42,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "3",
+      // _id: "3",
       name: "Lacoste Free Shirt",
       category: "Shirts",
       image: "/images/p3.jpg",
@@ -54,7 +54,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "4",
+      // _id: "4",
       name: "Nike Slim Pant",
       category: "Pants",
       image: "/images/p4.jpg",
@@ -66,7 +66,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "5",
+      // _id: "5",
       name: "Puma Slim Pant",
       category: "Pants",
       image: "/images/p5.jpg",
@@ -78,7 +78,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "6",
+      // _id: "6",
       name: "Adidas Fit Pant",
       category: "Pants",
       image: "/images/p6.jpg",
