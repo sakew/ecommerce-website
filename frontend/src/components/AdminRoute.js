7b46b401e0ac312a4310413d71934f1917c62e7b
sakew <dorinsecu@gmail.com>
2021-03-10 14:13:47 +0200

    renamed PrivateRoute to AdminRoute for the exported admin route function

--- frontend/src/components/AdminRoute.js
+++ frontend/src/components/AdminRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route } from "react-router";
 
-export default function PrivateRoute({ component: Component, ...rest }) {
+export default function AdminRoute({ component: Component, ...rest }) {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   return (
